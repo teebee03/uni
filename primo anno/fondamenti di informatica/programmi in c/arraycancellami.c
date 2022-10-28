24ebe1bc87b387d13111783d0c407fa6a336696d
teebee03 <tommasobertelli03@gmail.com>
2022-10-28 11:54:42 +0200

    it es and an1 files

---
+++ primo anno/fondamenti di informatica/programmi in c/arraycancellami.c
@@ -0,0 +1,6 @@
+/*
+in: array di interi
+out: array per cui il valore i-esimo 
+*/
+
+void array()
---
+++ primo anno/fondamenti di informatica/programmi in c/min_mul_09.c
@@ -0,0 +1,29 @@
+/*
+in: int
+out: intero positivo più piccolo multiplo del primo con cifre solo 0 o 9 
+*/
+
+#include<stdio.h>
+#include<limits.h>
+
+int min_mul(int);
+
+int main()
+{
+	int in;
+	
+	
+}
+
+int min_mul(int in)
+{
+	int mul,temp;
+	
+	
+	while(temp/%in!=0&&temp<MAX_INT)
+	{
+		temp
+	}
+	
+	return mul;
+}
